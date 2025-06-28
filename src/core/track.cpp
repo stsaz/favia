6c6077aee670dc1be99b913d7e73ffaf6e753b34
Simon Zolin <4729655+stsaz@users.noreply.github.com>
2025-06-28 08:02:26 +0300

    minor

--- src/core/track.cpp
+++ src/core/track.cpp
@@ -67,7 +67,7 @@ FF_EXTERN void tracks_init() {
 static int sdl_init_complete;
 
 static const char* time_print(uint64_t msec, char *buf, size_t cap) {
-	uint h = msec / 3600000,  m = msec / 60000,  s = (msec / 1000) % 60,  ms = msec % 1000;
+	uint h = msec / 3600000,  m = (msec / 60000) % 60,  s = (msec / 1000) % 60,  ms = msec % 1000;
 	ffsz_format(buf, cap, "%u:%02u:%02u.%03u"
 		, h, m, s, ms);
 	return buf;
